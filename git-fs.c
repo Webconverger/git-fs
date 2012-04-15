3c2c6a4221f2477987421ac65011fa6d54b776eb
Patrick Haller <patrick@haller.ws>
2012-04-15 13:53:30 +0800

    added subdir handling

--- git-fs.c
+++ git-fs.c
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <limits.h>
 #include <stdarg.h>
 
 #include <git2.h>
@@ -28,8 +29,6 @@ void debug(const char* format, ...) {
 git_index_entry *git_index_entry_by_file(const char *path) {
 	unsigned int i, ecount;
 	debug( "git_index_entry_by_file for %s\n", path);
-	path++;
-	debug( "  stripped to %s\n", path);
 	ecount = git_index_entrycount(g_index);
 	for (i = 0; i < ecount; ++i) {
 		git_index_entry *e = git_index_get(g_index, i);
@@ -42,18 +41,50 @@ git_index_entry *git_index_entry_by_file(const char *path) {
 	return NULL;
 }
 
+int is_dir(const char *path) {
+	int ecount, i;
+
+	debug("is_dir looking for '%s'\n", path);
+
+	if (strcmp(path, "") == 0) {
+		debug("  empty path is a dir, '%s'\n", path);
+		return 1;
+	}
+
+	ecount = git_index_entrycount(g_index);
+	for (i = 0; i < ecount; ++i) {
+		git_index_entry *e = git_index_get(g_index, i);
+		if (strncmp(path, e->path, strlen(path)) != 0) {
+			debug("  path '%s' not a substring of '%s'\n", path, e->path);
+			continue;
+		}
+		if (e->path[ strlen(path) ] == '/') {
+			debug("  prefix same, next char is slash for '%s' in '%s'\n", path, e->path);
+			return 1;
+		}
+		if (strcmp(path, e->path) == 0) {
+			debug("is_dir found file '%s'\n", e->path);
+			return 0;
+		}
+	}
+	debug("is_dir could not find path '%s'\n", path);
+	return 0;
+}
+
 int git_getattr(const char *path, struct stat *stbuf)
 {
+	path++;
 	memset(stbuf, 0, sizeof(struct stat));
-	if ( strcmp(path,"/") == 0 ) {
-		debug( "git_getattr for root -> %s\n", path);
+	debug("getattr stripped to '%s'\n", path);
+	if ( is_dir(path) ) {
+		debug( "git_getattr for dir -> %s\n", path);
 		stbuf->st_mode = 0040755;
 		stbuf->st_gid = 0;
 		stbuf->st_uid = 0;
 		stbuf->st_size = 0;
 		return 0;
 	}
-	debug( "git_getattr for %s\n", path);
+	debug( "  get_attr not a dir %s\n", path);
 	git_index_entry *e;
 	if ( (e = git_index_entry_by_file(path)) == NULL)
 		return debug("path no exist? %s\n", path),-ENOENT;
@@ -71,14 +102,35 @@ int git_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 {
 	(void) offset;
 	(void) fi;
-	int i,ecount = 0;
+	int i, ecount = 0;
+	char dir[PATH_MAX], file[PATH_MAX], *p, *basename;
 
+	path++; // drop initial slash
 	debug( "git_readdir for %s\n", path);
+	memset(dir, 0, sizeof(dir));
 	ecount = git_index_entrycount(g_index);
 	for (i = 0; i < ecount; ++i) {
 		git_index_entry *e = git_index_get(g_index, i);
-		debug( "readdir for '%s' got '%s'\n",path, e->path);
-		filler(buf, e->path, NULL, 0);
+		strncpy(file, e->path, strlen(e->path)+1);
+		debug( "  readdir for '%s' got '%s'\n",path, file);
+		if (strncmp(path, file, strlen(path)) != 0 && strlen(path) != 0) {
+			debug( "  readdir path '%s' not init substring of '%s'\n",path, file);
+			continue;
+		}
+		basename = (char *)file + strlen(path);
+		if ( *basename == '/')
+			basename++;
+		debug( "  readdir using basename '%s'\n", basename);
+		if ( (p = strchr(basename, '/')) == NULL) {
+			debug( "  readdir path '%s' and obj '%s' is basename, adding\n", path, basename);
+			filler(buf, basename, NULL, 0);
+			continue;
+		}
+		if (strncmp(dir, basename, p - basename) != 0) {
+			strncpy(dir, basename, p - basename);
+			debug( "  readdir path '%s' with basename '%s' and obj '%s' is new dir\n", path, basename, dir);
+			filler(buf, dir, NULL, 0);
+		}
 	}
 
 	return 0;
@@ -86,6 +138,7 @@ int git_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 
 int git_open(const char *path, struct fuse_file_info *fi)
 {
+	path++;
 	return 0;
 }
 
@@ -97,6 +150,7 @@ int git_read(const char *path, char *buf, size_t size, off_t offset,
 	git_odb_object *obj;
 	git_oid oid;
 
+	path++;
 	debug("git_read for -> %s with offset %d size %d\n", path, (int)offset, (int)size );
 
 	if ( (e = git_index_entry_by_file(path)) == NULL)
@@ -131,30 +185,23 @@ struct fuse_operations git_oper = {
 int main(int argc, char *argv[])
 {
 	char *r,*m;
-	char *a[]= { argv[0], "-f", argv[2] };
-	//git_oid g_oid;
-	//char *o;
+	char *args[]= { argv[0], argv[2] };
+	char *debug_args[] = { argv[0], "-f", argv[2] };
 
 	r = argv[1];
 	m = argv[2];
-	//o = argv[3];
-	//printf("using repo %s, mnt %s, oid %s\n",r, m, o);
-	printf("mounting repo %s on %s\n",r, m);
+	debug("mounting repo %s on %s\n",r, m);
 
 	if (git_repository_open(&g_repo, r) < 0)
 		return perror("repo"), 1;
 
-	//if ( git_oid_fromstrn(&g_oid, o, strlen(o)) < 0)
-	//	return debug( "oid %s -> %s", o, strerror(errno)),1;
-
-	/* this fails for some reason?
-	 * if ( git_tree_lookup(&g_tree, g_repo, &g_oid) < 0)
-		return perror("tree lookup"),1;
-		*/
 	git_repository_index(&g_index, g_repo);
 	git_index_read(g_index);
 	git_repository_odb(&g_odb, g_repo);
 
-	return fuse_main(3, a, &git_oper);
+	if (getenv("DEBUG") != NULL)
+		return fuse_main(3, debug_args, &git_oper);
+
+	return fuse_main(2, args, &git_oper);
 }
 
